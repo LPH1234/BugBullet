4abff4c517c95ff091db2802e0a1151e3bb20654
LCiZY <1443351569@qq.com>
2021-06-10 17:07:21 +0800

    渲染模块1.0

--- GameDemo/MyGame.cpp
+++ GameDemo/MyGame.cpp
@@ -1,9 +1,11 @@
-#include <ctype.h>
+﻿#include <ctype.h>
 
 #include "PxPhysicsAPI.h"
 #include "../Common/Print.h"
 #include "../Common/PVD.h"
 #include "../Utils/Utils.h"
+#include "objLoader.h"
+
 #define PI 3.1415926
 
 using namespace physx;
@@ -12,18 +14,20 @@ PxDefaultAllocator		gAllocator;
 PxDefaultErrorCallback	gErrorCallback;
 
 PxFoundation*			gFoundation = NULL;
-PxPhysics*				gPhysics	= NULL;
+PxPhysics*				gPhysics = NULL;
+
+PxCooking*				gCooking = NULL;
 
 PxDefaultCpuDispatcher*	gDispatcher = NULL;
-PxScene*				gScene		= NULL;
+PxScene*				gScene = NULL;
 
-PxMaterial*				gMaterial	= NULL;
+PxMaterial*				gMaterial = NULL;
 
-PxPvd*                  gPvd        = NULL;
+PxPvd*                  gPvd = NULL;
 
 PxReal stackZ = 10.0f;
 
-PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity=PxVec3(0))
+PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0))
 {
 	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
 	dynamic->setAngularDamping(0.5f);
@@ -35,11 +39,11 @@ PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, 
 void createStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
 {
 	PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
-	for(PxU32 i=0; i<size;i++)
+	for (PxU32 i = 0; i < size; i++)
 	{
-		for(PxU32 j=0;j<size-i;j++)
+		for (PxU32 j = 0; j < size - i; j++)
 		{
-			PxTransform localTm(PxVec3(PxReal(j*2) - PxReal(size-i), PxReal(i*2+1), 0) * halfExtent);
+			PxTransform localTm(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 + 1), 0) * halfExtent);
 			PxRigidDynamic* body = gPhysics->createRigidDynamic(t.transform(localTm));
 			body->attachShape(*shape);
 			PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
@@ -53,12 +57,12 @@ void createWall() {
 	PxTransform original(PxVec3(PxReal(0), PxReal(0), PxReal(0)));
 
 	PxShape* shape1 = gPhysics->createShape(PxBoxGeometry(100, 30, 3), *gMaterial);
-	PxShape* shape2 = gPhysics->createShape(PxBoxGeometry(3, 30, 100-3), *gMaterial);
+	PxShape* shape2 = gPhysics->createShape(PxBoxGeometry(3, 30, 100 - 3), *gMaterial);
 
 	PxTransform local1(PxVec3(PxReal(0), PxReal(30), PxReal(-100)));
-	PxTransform local2(PxVec3(PxReal(100-3), PxReal(30), PxReal(0)));
+	PxTransform local2(PxVec3(PxReal(100 - 3), PxReal(30), PxReal(0)));
 	PxTransform local3(PxVec3(PxReal(0), PxReal(30), PxReal(100)));
-	PxTransform local4(PxVec3(PxReal(-200), PxReal(30), PxReal(0)), PxQuat(0,1,0,0));
+	PxTransform local4(PxVec3(PxReal(-200), PxReal(30), PxReal(0)), PxQuat(0, 1, 0, 0));
 
 	PxRigidStatic* body1 = gPhysics->createRigidStatic(original.transform(local1));
 	body1->attachShape(*shape1);
@@ -73,7 +77,7 @@ void createWall() {
 	body4->attachShape(*shape1);
 	gScene->addActor(*body4);/**/
 	bodyToRemove = body4;
-	
+
 	shape1->release();
 	shape2->release();
 }
@@ -110,25 +114,37 @@ void createBowl() {
 	bowlShape->release();
 }
 
+void createModel(std::string path) {
+#if 0
+	ObjLoader loader(path, gPhysics, gCooking, gScene, gMaterial);
+	loader.createObjMeshs(PxVec3(PxReal(-5.5), PxReal(2), PxReal(0)), 1);
+#endif
+	ObjLoader loader(path, gPhysics, gCooking, gScene, gMaterial);
+	//loader.writeMeshCookingFile(1);
+	loader.createObjMeshs(PxVec3(PxReal(-5.5), PxReal(2), PxReal(0)), 1);
+}
+
 void initPhysics(bool interactive)
 {
 	gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback);
 
 	gPvd = PxCreatePvd(*gFoundation);
 	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
-	gPvd->connect(*transport,PxPvdInstrumentationFlag::eALL);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
 
-	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(),true,gPvd);
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	gCooking = PxCreateCooking(PX_PHYSICS_VERSION, *gFoundation, PxCookingParams(PxTolerancesScale()));
 
 	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
 	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
 	gDispatcher = PxDefaultCpuDispatcherCreate(2);
-	sceneDesc.cpuDispatcher	= gDispatcher;
-	sceneDesc.filterShader	= PxDefaultSimulationFilterShader;
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
 	gScene = gPhysics->createScene(sceneDesc);
 
 	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
-	if(pvdClient)
+	if (pvdClient)
 	{
 		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
 		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
@@ -136,47 +152,48 @@ void initPhysics(bool interactive)
 	}
 	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
 
-	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0,1,0,0), *gMaterial);
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
 	gScene->addActor(*groundPlane);
 
-	for(PxU32 i=0;i<5;i++)
-		createStack(PxTransform(PxVec3(0,0,stackZ-=10.0f)), 10, 2.0f);
+	for (PxU32 i = 0; i < 5; i++)
+		createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);
 
 	createWall();
 	createBowl();
+	createModel("D:/repo/PhysX-3.4-master/PhysX_3.4/GameDemo/compiler/vc15win64/module/lowBuilding/low-poly-buildings.obj");
 
-	if(!interactive)
-		createDynamic(PxTransform(PxVec3(0,40,100)), PxSphereGeometry(10), PxVec3(0,-50,-100));
+	if (!interactive)
+		createDynamic(PxTransform(PxVec3(0, 40, 100)), PxSphereGeometry(10), PxVec3(0, -50, -100));
 }
 
 void stepPhysics(bool interactive)
 {
 	PX_UNUSED(interactive);
-	gScene->simulate(1.0f/60.0f);
+	gScene->simulate(1.0f / 60.0f);
 	gScene->fetchResults(true);
 }
-	
+
 void cleanupPhysics(bool interactive)
 {
 	PX_UNUSED(interactive);
 	gScene->release();
 	gDispatcher->release();
-	gPhysics->release();	
+	gPhysics->release();
 	PxPvdTransport* transport = gPvd->getTransport();
 	gPvd->release();
 	transport->release();
-	
+
 	gFoundation->release();
-	
+
 	printf("SnippetHelloWorld done.\n");
 }
 
 void keyPress(unsigned char key, const PxTransform& camera)
 {
-	switch(toupper(key))
+	switch (toupper(key))
 	{
-	case 'B':	createStack(PxTransform(PxVec3(0,0,stackZ-=10.0f)), 10, 2.0f);						break;
-	case ' ':	createDynamic(camera, PxSphereGeometry(3.0f), camera.rotate(PxVec3(0,0,-1))*200);	break;
+	case 'B':	createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);						break;
+	case ' ':	createDynamic(camera, PxSphereGeometry(3.0f), camera.rotate(PxVec3(0, 0, -1)) * 200);	break;
 	case 'R':	remove();	break;
 	}
 }
@@ -189,7 +206,7 @@ int snippetMyMain(int, const char*const*)
 #else
 	static const PxU32 frameCount = 100;
 	initPhysics(false);
-	for(PxU32 i=0; i<frameCount; i++)
+	for (PxU32 i = 0; i < frameCount; i++)
 		stepPhysics(false);
 	cleanupPhysics(false);
 #endif
---
+++ GameDemo/objLoader.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <vector>
+#include <iostream> 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+#include <list>
+
+#include "PxPhysicsAPI.h"
+#include "geometry/PxTriangleMesh.h"
+#include "geometry/PxTriangleMeshGeometry.h"
+#include "foundation/PxPreprocessor.h"
+
+
+using namespace physx;
+
+
+class ObjLoader
+{
+public:
+	// 几何体顶点v
+	struct vertices {
+		float x;
+		float y;
+		float z;
+	};
+	// 面f
+	struct face {
+		unsigned int u; //顶点标号
+		unsigned int v; // 贴图顶点标号
+		unsigned int w;	// 法向量标号
+	};
+	ObjLoader(std::string &filename, PxPhysics* gPhysics, PxCooking*	gCooking, PxScene* gScene, PxMaterial* gMaterial);//读取函数
+	~ObjLoader();
+	//void obj_parse_face(const string& szface, struct face facesrc);
+private:
+	physx::PxTriangleMesh* createOjbMesh(int scale);
+	PxPhysics* gPhysics;
+	PxCooking*	gCooking;
+	PxScene* gScene;
+	PxMaterial* gMaterial;
+
+public:
+	std::vector<struct vertices> v;//存放顶点(x,y,z)坐标
+	std::list<std::vector<struct face>> f;//存放面的三个顶点索引
+	std::string name;
+
+	physx::PxRigidActor* createObjMeshs(physx::PxVec3 &offset, int scale);
+
+	int writeMeshCookingFile(int scale);
+	PxTriangleMesh * readCookingFile();
+};
