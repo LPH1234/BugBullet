2f188b43c4806a77a8fae897bba79ebd860001d6
Hellion12 <463907194@qq.com>
2021-06-16 14:23:46 +0800

    更改刚体生成函数 更改模型mtl和贴图

--- Render/Model.h
+++ Render/Model.h
@@ -105,7 +105,6 @@ private:
 		// walk through each of the mesh's vertices
 		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
 		{
-			cout << "count:" << count++ << "\n";
 			Vertex vertex;
 			glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
 			// positions
@@ -203,7 +202,9 @@ private:
 			if (!skip)
 			{   // if texture hasn't been loaded already, load it
 				Texture texture;
+				cout << "准备加载："<<"\t"<< str.C_Str() << "\t" << this->directory << endl;
 				texture.id = TextureFromFile(str.C_Str(), this->directory);
+				cout << "加载完成"<< endl; 
 				texture.type = typeName;
 				texture.path = str.C_Str();
 				textures.push_back(texture);
