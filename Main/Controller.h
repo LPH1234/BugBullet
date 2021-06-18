b0b60ebc6ff276d1c470e9a77e4783db249b0feb
LCiZY <1443351569@qq.com>
2021-06-18 09:36:33 +0800

    add controller class

--- Main/Controller.h
+++ Main/Controller.h
@@ -8,9 +8,35 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <iostream>
 #include <vector>
+#include<unordered_map>
+
+#include "PxPhysicsAPI.h"
+
+#include "../Render/Render.h"
+#include "../Render/Camera.h"
+
+#include "../Render/models.h"
+#include "../Data/Consts.h"
 
 extern PxRigidDynamic* player_ctl;
+extern std::unordered_map<int, bool> keyToPressState;
+extern std::unordered_map<int, bool> keyToPrePressState;
+
+
+
+void keypress();
+void mouseMove();
+
+
+
+void vehicleProcessKeyboard();
+
+
+
+
+
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum PlayerMovement {
 	Player_FORWARD,
